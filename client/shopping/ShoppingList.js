1057c698f2e1e7c8a38faf8892a65b83ff59454f
Carlos Gomez <carlosgomez.deb@gmail.com>
2018-02-06 12:46:50 -0600

    move menu to own folder, created shopping list, added routes to shopping list

---
+++ client/shopping/ShoppingList.js
@@ -0,0 +1,13 @@
+// Load Shopping Items
+Template.ShoppingList.onCreated(function () {
+  let self = this
+  self.autorun(function () {
+    self.subscribe('recipes')
+  })
+})
+
+Template.ShoppingList.helpers({
+  shoppingList: () => {
+    return Recipes.find({ inMenu: true })
+  }
+})

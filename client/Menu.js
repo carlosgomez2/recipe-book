2e94fd7390b5ea7e713e1da0a4351344387dfe77
Carlos Gomez <carlosgomez.deb@gmail.com>
2018-02-06 12:14:13 -0600

    filtering menu items

---
+++ client/Menu.js
@@ -0,0 +1,13 @@
+// Load Recipes Subscribe
+Template.Menu.onCreated(function () {
+  let self = this
+  self.autorun(function () {
+    self.subscribe('recipes')
+  })
+})
+
+Template.Menu.helpers({
+  recipes: () => {
+    return Recipes.find({inMenu: true})
+  }
+})
